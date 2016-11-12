983f5b519f09b1814c8f8ac0c19e85e4e9d7569c
Clapa Lucian <lucian@dealdash.com>
2016-11-12 11:42:08 +0200

    add main presenter

---
+++ app/src/main/java/com/clpstudio/tvshowtimespent/presentation/mainscreen/IMainScreenPresenter.java
@@ -0,0 +1,15 @@
+package com.clpstudio.tvshowtimespent.presentation.mainscreen;
+
+import com.clpstudio.tvshowtimespent.general.mvp.abstraction.IBaseMvpPresenter;
+
+/**
+ * Created by clapalucian on 11/12/16.
+ */
+
+public interface IMainScreenPresenter extends IBaseMvpPresenter<IMainScreenPresenter.View> {
+
+    public interface View {
+
+    }
+
+}
---
+++ app/src/main/java/com/clpstudio/tvshowtimespent/presentation/mainscreen/MainScreenPresenter.java
@@ -0,0 +1,13 @@
+package com.clpstudio.tvshowtimespent.presentation.mainscreen;
+
+import com.clpstudio.tvshowtimespent.general.mvp.abstraction.BaseMvpPresenter;
+
+/**
+ * Created by clapalucian on 11/12/16.
+ */
+
+public class MainScreenPresenter extends BaseMvpPresenter<IMainScreenPresenter.View> implements IMainScreenPresenter {
+
+
+
+}
