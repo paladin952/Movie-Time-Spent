add922ac9cda39e7f82e927a4103e76db506608c
Clapa Lucian <lucian@dealdash.com>
2017-01-14 17:14:36 +0200

    add chart

--- app/src/main/java/com/clpstudio/tvshowtimespent/presentation/detailscreen/DetailActivity.java
+++ app/src/main/java/com/clpstudio/tvshowtimespent/presentation/detailscreen/DetailActivity.java
@@ -2,8 +2,10 @@ package com.clpstudio.tvshowtimespent.presentation.detailscreen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,7 +15,16 @@ import com.clpstudio.tvshowtimespent.TvShowApplication;
 import com.clpstudio.tvshowtimespent.general.utils.Constants;
 import com.clpstudio.tvshowtimespent.general.utils.UrlConstants;
 import com.clpstudio.tvshowtimespent.presentation.model.DbTvShow;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +38,9 @@ public class DetailActivity extends AppCompatActivity {
 
     @BindView(R.id.title)
     TextView title;
+
+    @BindView(R.id.pie_char)
+    PieChart pieChart;
 
     public static void startActivity(Activity activity, DbTvShow dbTvShow) {
         Intent intent = new Intent(activity, DetailActivity.class);
@@ -47,6 +61,8 @@ public class DetailActivity extends AppCompatActivity {
 
         tvShow = (DbTvShow) getIntent().getExtras().getSerializable(Constants.Intents.KEY_TV_SHOW);
         setupUi();
+
+        setData();
     }
 
     private void setupUi() {
@@ -71,5 +87,109 @@ public class DetailActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        setPieChart();
     }
+
+    private void setPieChart() {
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+//        pieChart.setCenterTextTypeface(mTfLight);
+        pieChart.setCenterText(generateCenterSpannableText());
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+
+        pieChart.setTransparentCircleColor(Color.WHITE);
+        pieChart.setTransparentCircleAlpha(110);
+
+        pieChart.setHoleRadius(58f);
+        pieChart.setTransparentCircleRadius(61f);
+
+        pieChart.setDrawCenterText(true);
+
+        pieChart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        pieChart.setRotationEnabled(true);
+        pieChart.setHighlightPerTapEnabled(true);
+
+        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+
+        // entry label styling
+        pieChart.setEntryLabelColor(Color.WHITE);
+//        pieChart.setEntryLabelTypeface(mTfRegular);
+        pieChart.setEntryLabelTextSize(12f);
+
+    }
+
+    private SpannableString generateCenterSpannableText() {
+
+        SpannableString s = new SpannableString("Seasons");
+//        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+//        s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
+//        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
+//        s.setSpan(new RelativeSizeSpan(.8f), 14, s.length() - 15, 0);
+//        s.setSpan(new StyleSpan(Typeface.ITALIC), s.length() - 14, s.length(), 0);
+//        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
+        return s;
+    }
+
+    private void setData() {
+
+        int range = Integer.parseInt(tvShow.getNumberOfSeasons());
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < range ; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * range) + range / 5), String.valueOf(i)));
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, getString(R.string.episodes));
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
+        //dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+//        data.setValueTypeface(mTfLight);
+        pieChart.setData(data);
+
+        // undo all highlights
+        pieChart.highlightValues(null);
+
+        pieChart.invalidate();
+    }
+
 }
